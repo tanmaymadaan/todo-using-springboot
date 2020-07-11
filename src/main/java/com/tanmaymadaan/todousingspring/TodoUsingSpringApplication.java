110728d4bc1910028a4103a1c97b5cc7103c80e8
Tanmay Madaan <tanmaymadaan@outlook.com>
2020-07-11 10:48:19 +0530

    Init SpringBoot

---
+++ src/main/java/com/tanmaymadaan/todousingspring/TodoUsingSpringApplication.java
@@ -0,0 +1,13 @@
+package com.tanmaymadaan.todousingspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoUsingSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoUsingSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tanmaymadaan/todousingspring/TodoUsingSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tanmaymadaan.todousingspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoUsingSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
